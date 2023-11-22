f8dd2e8aa7c64694626fe7cf5b2fa62f9a293cc2
lkds <utilitymail988@gmail.com>
2023-11-22 15:19:30 +0530

    resolved issue with insertionSort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -5,11 +5,18 @@
 
 using namespace std;
 
-void insertionSort(vector <int> &data){
-
+void insert(vector <int> &data, int index){
+    
 }
 
-
+void insertSort(vector <int> &data){
+    for(int i=0; i<data.size()-1; i++){
+        if(data[i]>data[i+1]){
+            insert(data, i+1);
+            
+        }
+    }
+}
 
 int main(){
 
